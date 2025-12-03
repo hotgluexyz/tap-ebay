d09f041810b1c5b9cab90d713d75303f7f9f569a
Lior Ben-David <liorbd@Outlook.com>
2025-12-03 11:30:47 -0500

    Support sandbox (#2)

--- tap_ebay/auth.py
+++ tap_ebay/auth.py
@@ -4,6 +4,9 @@
 from hotglue_tap_sdk.authenticators import OAuthAuthenticator, SingletonMeta
 
 
+PROD_TOKEN_ENDPOINT = "https://api.ebay.com/identity/v1/oauth2/token"
+SANDBOX_TOKEN_ENDPOINT = "https://api.sandbox.ebay.com/identity/v1/oauth2/token"
+
 class EbayAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
     """Authenticator class for Ebay."""
 
@@ -19,11 +22,14 @@ class EbayAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
         """Return the authentication credentials for the request."""
         return (self.config["client_id"], self.config["client_secret"])
 
+
     @classmethod
     def create_for_stream(cls, stream) -> "EbayAuthenticator":
+        is_sandbox = stream._tap.config.get("is_sandbox", False)
+        
         return cls(
             stream=stream,
             config_file=stream._tap.config,
-            auth_endpoint="https://api.ebay.com/identity/v1/oauth2/token",
+            auth_endpoint=SANDBOX_TOKEN_ENDPOINT if is_sandbox else PROD_TOKEN_ENDPOINT,
             oauth_scopes=None,
         )
